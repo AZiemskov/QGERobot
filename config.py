1a25bde573d3a5fc966dd45b97d1fdde249bb443
Contr1ver <externalrock@gmail.com>
2020-02-10 22:25:49 +0200

    Чтото наваял

---
+++ config.py
@@ -0,0 +1,4 @@
+"""
+rythrtjhrghfgh
+"""
+TOKEN = '1048962908:AAG8CbKkxK55qQHTlXLVN4gZslcB9zwjFl4'
---
+++ main.py
@@ -0,0 +1,40 @@
+"""
+dfghjklkjhgfghjk
+"""
+import telebot
+from telebot.types import Message
+
+import config
+import game_text
+import inline_keyboard_button
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(commands=["start"])
+def send_welcome(message: Message):
+    """
+    :param message:
+    :return:
+    """
+    bot.send_message(
+        message.chat.id,
+        "Привет первый игрок, {}".format(message.chat.first_name, message.text)
+    )
+
+
+@bot.message_handler(content_types=["text"], func=lambda message: True)
+def start_game_text(message: Message):
+    """
+    :param message:
+    :return:
+    """
+    bot.send_message(
+        message.chat.id,
+        game_text.PIZZA["start game"].format(message.chat.first_name, message.text),
+        reply_markup=inline_keyboard_button.get_base_inline_keyboard()
+    )
+
+
+if __name__ == "__main__":
+    bot.infinity_polling()
